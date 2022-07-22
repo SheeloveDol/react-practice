e6a3e59d3ed106965eb3fe71842b257c137ab613
Sheelove Dol <siesel26@gmail.com>
2022-07-22 16:50:13 -0700

    created header component

--- src/App.js
+++ src/App.js
@@ -5,18 +5,12 @@ import './App.css';
 // creating a component
 const greetings = 'Hello to you all';
 
-const comments = [
-  {title: 'One', text: 'I am comment 1'},
-  {title: 'Two', text: 'I am comment 2'},
-  {title: 'Three', text: 'I am comment 3'},
-
-]
 
 function App() {
   return (
 
     <fragment>
-      <Header text = "Revision"/>
+      <Header text = "Revision" another = "Of React" greet = {greetings} />
       <div>
         <h1>{greetings}</h1>
       </div>
--- src/components/header/Header.component.jsx
+++ src/components/header/Header.component.jsx
@@ -1,13 +1,20 @@
 
 
-const Header = ({text}) => {
+const Header = ({inCase, text, another, greet}) => {
     return (
         <header>
             <div className="container">
                 <h1>{text}</h1>
+                <h2>{another}</h2>
+                <h4>{inCase}</h4>
+                <p>{greet}</p>
+
             </div>
         </header>
     )
 }
 
+Header.defaultProps = {
+    inCase: 'This is a default prop'
+}
 export default Header
